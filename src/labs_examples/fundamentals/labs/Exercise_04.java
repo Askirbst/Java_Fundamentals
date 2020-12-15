b22e7d2dd23ae5de3e0800cdad5817f7349d8a83
Askirbst <tsbriks@gmail.com>
2020-12-15 12:04:54 -0500

    Fundamentals_labs_Exercises

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -13,9 +13,22 @@ public class Exercise_04 {
     public static void main(String[] args) {
 
         int i = 1;
-        System.out.println("int i is: " + i);
+        boolean success = false;
+        byte b = 127;
+        short c = 23767;
+        char d = 'd';
+        long e = 123456789101112L;
+        float f = 123456789123456.12345f;
+        double g = 123456789123456789123456798789789789789789.12345;
 
-        // write your code below
+        System.out.println("int i is: " + i);
+        System.out.println("boolean success is: " + success);
+        System.out.println("byte b is: " + b);
+        System.out.println("short c is: " + c);
+        System.out.println("char d is: " + d);
+        System.out.println("long e is: " + e);
+        System.out.println("float f is: " + f);
+        System.out.println("double g is: " + g);
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -12,17 +12,23 @@ public class Exercise_05 {
 
     public static void main(String[] args) {
 
-        String str = "hello!";
-        // please declare an int variable below, and set it to the value of the length of "str"
+        String str1 = "hello!";
+        // please declare an int variable below, and set it to the value of the length of "str1"
 
         String str2 = "hello";
-        // please initialize a boolean variable and test whether str is equal to str2
+        // please initialize a boolean variable and test whether str1 is equal to str2
+        boolean equalStrings = str1.equalsIgnoreCase(str2);
+
+        System.out.println("The length of str1 is " + str1.length());
+        System.out.println("The length of str2 is " + str2.length());
+        System.out.println("Do str1 and str2 match?" + equalStrings);
 
         // please concatenate str & str2 and set the result to a new String variable below
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
-
+        System.out.println("The length of str1 and str2 is ");
+        System.out.println(str1.length() + str2.length());
     }
 
 
