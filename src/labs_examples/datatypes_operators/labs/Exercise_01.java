022aa4c597f5323dd85ee2d97e3b582734e26890
Alex Skirbst <tsbriks@gmail.com>
2020-12-21 21:27:42 -0500

    Deleting

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -15,6 +15,13 @@ package labs_examples.datatypes_operators.labs;
 public class Exercise_01 {
 
     public static void main(String[] args) {
+        int a = 1;
+        double b = 2;
+        float c = 2.22f;
+        System.out.printf("Value of int a = " + a);
+        System.out.println("");
+        System.out.println("Value of double b = " + b);
+        System.out.println("Value of float c = " + c);
 
         // write code here
 
