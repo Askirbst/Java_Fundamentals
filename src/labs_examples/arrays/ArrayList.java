fa53809d34e55318d99d4b0edba5a36490fbc623
Askirbst <tsbriks@gmail.com>
2021-01-21 15:58:27 -0500

    ArrayList

--- src/labs_examples/arrays/ArrayList.java
+++
@@ -1,9 +0,0 @@
-package labs_examples.arrays;
-
-public class ArrayList {
-    public static void main(String[] args) {
-        // create new ArrayList of String
-
-        ArrayList<String>
-    }
-}
---
+++ src/labs_examples/arrays/ArrayListExample.java
@@ -0,0 +1,28 @@
+package labs_examples.arrays;
+
+import java.util.ArrayList;
+
+public class ArrayExampleList {
+    public static void main(String[] args) {
+        // create new ArrayList of String
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Hello");
+        arrayList.add("World");
+        arrayList.add("Programming");
+        arrayList.add("is");
+        arrayList.add("so");
+        arrayList.add("cool");
+
+        String val = arrayList.get(4);
+        System.out.println(val);
+
+        boolean check = arrayList.contains("Programming");
+        System.out.println(check);
+
+        for(String s : arrayList){
+            System.out.println(s);
+        }
+    }
+}
