612d110a32dd82bba9f2ec925e96a10075133b6b
Askirbst <tsbriks@gmail.com>
2021-01-26 20:12:36 -0500

    Arrays_Labs_Exercise01

---
+++ src/labs_examples/arrays/ArraysQuiz.java
@@ -0,0 +1,43 @@
+package labs_examples.arrays;
+
+import java.util.ArrayList;
+
+public class ArraysQuiz {
+    public static void main(String[] args) {
+
+        //YOUR CODE BELOW
+
+        /*  1.  First create and initialize an ArrayList called guests.
+                Remember, the syntax to create an ArrayList is:
+                ArrayList<type> name = new ArrayList<type>();   */
+        ArrayList<String> guests = new ArrayList<>();
+
+        /*  2.  Now add the following names to the list using .add() :
+                Arnold, Gerald, Eugene, Helga, Phoebe in that order */
+        guests.add("Arnold");
+        guests.add("Gerald");
+        guests.add("Eugene");
+        guests.add("Helga");
+        guests.add("Phoebe");
+
+        /*  3.  Print the names using a ForEach loop.
+                Remember, the syntax for the ForEach loop is:
+                for(type element: collection) { code }          */
+        for (String s : guests) {
+            System.out.println(s);
+            
+        }
+
+        /*  4.  Eugene wont be able to make it.
+                Remove him from the list using .remove() */
+        guests.remove("Eugene");
+
+        /*  5. Print the list again  */
+        for (String s : guests) {
+            System.out.println(s);
+
+        }
+
+        //YOUR CODE ABOVE}
+    }
+}
--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,9 +1,12 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  * Arrays calculator
  *
- *      Take in 10 numbers from the user. Place the numbers in an array. Using the loop of your choice,
+ *      Take in 10 numbers from the user. Place the numbers in an array.
+ *      Using the loop of your choice,
  *      calculate the sum of all of the numbers in the array as well as the average.
  *
  *      Print the results to the console.
@@ -11,5 +14,34 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_01 {
-
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner s1 = new Scanner(System.in);
+        Scanner s2 = new Scanner(System.in);
+        Scanner s3 = new Scanner(System.in);
+        Scanner s4 = new Scanner(System.in);
+        Scanner s5 = new Scanner(System.in);
+        Scanner s6 = new Scanner(System.in);
+        Scanner s7 = new Scanner(System.in);
+        Scanner s8 = new Scanner(System.in);
+        Scanner s9 = new Scanner(System.in);
+        Scanner s10 = new Scanner(System.in);
+        System.out.println("Enter 10 numbers to calculate sum: ");
+        int[] nums = new int[10];
+        nums[0] = s1.nextInt();
+        nums[1] = s2.nextInt();
+        nums[2] = s3.nextInt();
+        nums[3] = s4.nextInt();
+        nums[4] = s5.nextInt();
+        nums[5] = s6.nextInt();
+        nums[6] = s7.nextInt();
+        nums[7] = s8.nextInt();
+        nums[8] = s9.nextInt();
+        nums[9] = s10.nextInt();
+        for(int i = 0; i < 10; i++){
+            sum+=nums[i];
+        }
+        System.out.println("The sum is: " + sum);
+        System.out.println("The average is: " + sum/10);
+    }
 }
