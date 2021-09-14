6ad085deed2ff778979616452c05d06ea959d560
Askirbst <tsbriks@gmail.com>
2021-09-14 13:23:15 -0400

    Exercise_03_continued

--- src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
+++ src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
@@ -2,9 +2,11 @@ package labs_examples.objects_classes_methods.labs.methods;
 
 import java.util.Scanner;
 
+import java.util.Arrays;
+
 public class MethodTraining {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Method sum() is an example of method overloading
         int add1 = sum(1, 2, 3);
         System.out.println(add1);
@@ -23,34 +25,46 @@ public class MethodTraining {
 
         //Method max() will return the largest of 4 numbers
         int num = max(18, 99, 54, 12);
-        System.out.println(num);
+        System.out.println("The largest of these numbers is : " + num);
 
-        System.out.println("Enter word to return number of consonants: ");
+        System.out.println("Enter phrase to return number of consonants: ");
         int count = consonants();
         System.out.println("Number of consonants in given phrase is: " + count);
+
+        System.out.println("Enter number to determine if prime: ");
+        prime();
+
+        int[] a = numbers(0,1,56,23);
+
+        System.out.println("The minimum and maximum values in the array, respectively, are : " + Arrays.toString(a));
+
     }
 
-    public static int sum(int a, int b, int c){
+    public static int sum(int a, int b, int c) {
+        System.out.print("The sum of " + a + " + " + b + " + " + c + " is: ");
         return a + b + c;
 
     }
 
-    public static int sum(int a, int b, int c, int d){
+    public static int sum(int a, int b, int c, int d) {
+        System.out.print("The sum of " + a + " + " + b + " + " + c + " + " + d + " is: ");
         return a + b + c + d;
 
     }
 
-    public static float sum(float a, float b, float c){
+    public static float sum(float a, float b, float c) {
+        System.out.print("The sum of " + a + " + " + b + " + " + c + " is: ");
         return a + b + c;
 
     }
+
     public static int pass(int val) {
         val = val * 2;
         return val;
 
     }
 
-    public static int max(int a , int b, int c, int d){
+    public static int max(int a, int b, int c, int d) {
         int[] array = new int[4];
         array[0] = a;
         array[1] = b;
@@ -58,32 +72,84 @@ public class MethodTraining {
         array[3] = d;
 
         int max = array[0];
-        for(int i=1; i < 4; i++) {
+        for (int i = 1; i < 4; i++) {
             if (array[i] > max)
                 max = array[i];
 
-        }return max;
+        }
+
+        return max;
 
     }
 
-    public static int consonants(){
+    public static int consonants() {
 
         Scanner word = new Scanner(System.in);
         String scn = word.nextLine();
 
         int count = 0;
-        for (int i = 0; i < scn.length(); i++){
+        for (int i = 0; i < scn.length(); i++) {
             char ch = scn.charAt(i);
-            if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' && ch != ' ') {
+            if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' && ch != ' ' && ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U' && ch != '!' && ch != '.' && ch != ',' && ch != '?') {
                 count++;
 
             }
 
-        }return count;
+        }
+
+        return count;
+
+    }
+
+    public static void prime() {
+
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        int i, m, flag = 0;
+        m = num / 2;
+        if (num == 0 || num == 1) {
+            System.out.println(num + " is not prime :(");
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (num % i == 0) {
+                    System.out.println(num + " is not prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println(num + " is prime!");
+            }
+        }
+
+    }
+
+    public static int[] numbers(int a, int b, int c, int d) {
+        int[] array = new int[4];
+        int min1 = a;
+        int max1 = a;
+
+        array[0] = a;
+        array[1] = b;
+        array[2] = c;
+        array[3] = d;
+
+        for (int i = 0; i < 4; i++) {
+            if (array[i] < min1)
+                min1 = array[i];
+            if (array[i] > max1)
+                max1 = array[i];
+
+        }
+        int[] minmax = new int[2];
+        minmax[0] = min1;
+        minmax[1] = max1;
+        return minmax;
 
     }
 
 }
+
 class Person {
     String name;
     int age;
