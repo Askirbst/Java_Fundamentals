9d005f83d63b8a539427ac5e4b1997cf9aa3211b
Askirbst <tsbriks@gmail.com>
2021-09-30 13:12:12 -0400

    Inheritance_Exercise_02

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -4,7 +4,8 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Run the class below and study the output. What prints and why?
  *
  * Your answer:
- *
+ * The value 10 prints because a new object of class B is created but with reference to the A class.
+ * So any call of object a will reference fields in class A.
  */
 class A {
     int i = 10;
